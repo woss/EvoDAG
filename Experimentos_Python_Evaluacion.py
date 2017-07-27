66e381bc8f94d76ef667a7f66d678e3622d59475
ClaudiaSanchez <claudianallely999@hotmail.com>
2017-07-27 12:35:37 -0500

    Experimentos_Python_Evaluacion.py modified

--- Experimentos_Python_Evaluacion.py
+++ Experimentos_Python_Evaluacion.py
@@ -15,8 +15,8 @@ def BER(y, yh):
 
 #res = ['EvoDAG','TF','TDOF','TF_params','TDOF_params']
 #folderRes = ['/shared/mgraffg/evodag-used-datasets/binary-C/evodag-0.10.6/','../res/evodag-0.10.6/TF/','../res/evodag-0.10.6/TDOF/','../res/evodag-0.10.6/TF_params/','../res/evodag-0.10.6/TDOF_params/']
-res = ['TDOF_params','TDOF10_params']
-folderRes = ['../res/evodag-0.10.6/TDOF_params/','../res/evodag-0.10.6/TDOF10_params/']
+res = ['TF_params','TDOF_params']
+folderRes = ['../res/evodag-0.10.6/TF_params/','../res/evodag-0.10.6/TDOF_params/']
 folderData = '../data/'
 datasets = ['thyroid','banana','titanic','diabetis','breast-cancer','flare-solar','heart','ringnorm','twonorm','german','waveform','splice','image']
 datasets_size = [100,100,100,100,100,100,100,100,100,100,100,20,20]
@@ -25,7 +25,7 @@ datasets_size = [100,100,100,100,100,100,100,100,100,100,100,20,20]
 os.chdir('/shared/cnsanchez/EvoDAG')
 #os.chdir('/home/up/Documents/DOCTORADO/CODIGO/EvoDAG')
 
-####### BER Table ###########################################
+####### BER and fitness Table ###########################################
 strv = 'Data set '
 for i in range(len(res)):
     strv += ' & '+res[i]
@@ -35,8 +35,8 @@ print('\hline')
 
 for i in range(len(datasets)):
     archivo = datasets[i]
-    
     strv = archivo
+
     for k in range(len(res)):
         ber = []
         for j in range(1,datasets_size[i]+1):
@@ -45,47 +45,32 @@ for i in range(len(datasets)):
             D1 = pandas.read_csv(filePredict,sep=",")
             D2 = pandas.read_csv(fileDataTestLabels,sep=",")
             ber.append(BER(D2.values,D1.values))
-        v = numpy.median( numpy.array(ber) )
-        strv += '& '+str( round(v,4))
-    print(strv + '\\\\ \\hline')
-print(' ')
+        ber_v = numpy.median( numpy.array(ber) )
+        strv += '& '+str( round(ber_v,4))
 
-####### Fitness Table ###########################################
-strv = 'Data set '
-for i in range(len(res)):
-    strv += ' & '+res[i]
-print('\hline')
-print(strv + '\\\\')
-print('\hline')
-
-for i in range(len(datasets)):
-    archivo = datasets[i]
-    
-    strv = archivo
     for k in range(len(res)):
-        value = []
+        fitness = []
         for j in range(1,datasets_size[i]+1):
             fileModel = folderRes[k]+archivo+'_test_data_'+str(j)+'.model'
             with gzip.open(fileModel, 'r') as fpt:
                 m = pickle.load(fpt)
-                value.append( m.fitness_vs )
-                #print("Height: %s" % m.height)
-                #print("Size: %s" % m.size)
-                #print("Fitness vs: %s" % m.fitness_vs)
-        v = numpy.median( numpy.array(value) )
-        strv += '& '+str( round(v,4))
+                fitness.append( m.fitness_vs )
+        fitness_v = numpy.median( numpy.array(fitness) )
+        strv += '& '+str( round(fitness_v,4))
     print(strv + '\\\\ \\hline')
 print(' ')
 
-####### Size Table ###########################################
+####### Fitness Table ###########################################
 # strv = 'Data set '
 # for i in range(len(res)):
 #     strv += ' & '+res[i]
 # print('\hline')
 # print(strv + '\\\\')
 # print('\hline')
+
 # for i in range(len(datasets)):
 #     archivo = datasets[i]
+    
 #     strv = archivo
 #     for k in range(len(res)):
 #         value = []
@@ -93,10 +78,11 @@ print(' ')
 #             fileModel = folderRes[k]+archivo+'_test_data_'+str(j)+'.model'
 #             with gzip.open(fileModel, 'r') as fpt:
 #                 m = pickle.load(fpt)
-#                 value.append( m.size )
+#                 value.append( m.fitness_vs )
 #                 #print("Height: %s" % m.height)
 #                 #print("Size: %s" % m.size)
 #                 #print("Fitness vs: %s" % m.fitness_vs)
 #         v = numpy.median( numpy.array(value) )
 #         strv += '& '+str( round(v,4))
 #     print(strv + '\\\\ \\hline')
+# print(' ')
